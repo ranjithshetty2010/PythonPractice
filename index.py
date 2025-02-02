3531fa484eca2dcd9e7136d4b796c5048772ed2d
Ranjith <ranjithshetty.2010@gmail.com>
2025-02-02 18:53:34 +0530

    First

---
+++ index.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import seaborn as sa 
+import numpy as np
+import pandas as pd
+import matplotlib as mt 
+
+
+def main():
+    st.title("This is first programme")   
+    st.sidebar.title("This is side bar")
+
+    uploadfile = st.sidebar.file_uploader("Upload file",type=["csv","xlsx"])
+
+    if uploadfile is not None:
+        if uploadfile.name.endswith(".csv"):
+           data=  pd.read_csv(uploadfile)
+        elif uploadfile.name.endswith(".xlsx"):
+          data =  pd.read_excel(uploadfile)
+         
+        st.subheader("file data")
+        st.dataframe(data.head())
+        
+
+
+    else:
+        print("Ouside Not a valid file")
+        
+if __name__ == "__main__":
+    main()
